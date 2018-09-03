ffdfcb0bb987b78dbd4bd53d4dc1d4b6de7ecfdb
Gabriel Ferreira <gabriel@lightup.com.br>
2018-09-03 14:06:09 -0300

    alterando versão firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.0.4/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.0.4/firebase-messaging.js');
 
 firebase.initializeApp({
     messagingSenderId: "817714672056"
